f65855f1883ef7d9e28ea1e1f15da33af607c1b9
Pahrijal Saban Mubarok <Kevinsdrajat27@gmail.com>
2023-11-24 14:24:47 +0700

    V1 - fix

--- src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -4,37 +4,37 @@ const InvariantError = require('../InvariantError');
 describe('DomainErrorTranslator', () => {
   it('should translate error correctly', () => {
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY')))
-      .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'));
+        .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'));
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION')))
-      .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'));
+        .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'));
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_LIMIT_CHAR')))
-      .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'));
+        .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'));
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
-      .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'));
+        .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'));
     expect(DomainErrorTranslator.translate(new Error('USER_LOGIN.NOT_CONTAIN_NEEDED_PROPERTY')))
-      .toStrictEqual(new InvariantError('harus mengirimkan username dan password'));
+        .toStrictEqual(new InvariantError('harus mengirimkan username dan password'));
     expect(DomainErrorTranslator.translate(new Error('USER_LOGIN.NOT_MEET_DATA_TYPE_SPECIFICATION')))
-      .toStrictEqual(new InvariantError('username dan password harus string'));
+        .toStrictEqual(new InvariantError('username dan password harus string'));
     expect(DomainErrorTranslator.translate(new Error('REFRESH_AUTHENTICATION_USE_CASE.NOT_CONTAIN_REFRESH_TOKEN')))
-      .toStrictEqual(new InvariantError('harus mengirimkan token refresh'));
+        .toStrictEqual(new InvariantError('harus mengirimkan token refresh'));
     expect(DomainErrorTranslator.translate(new Error('REFRESH_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
-      .toStrictEqual(new InvariantError('refresh token harus string'));
+        .toStrictEqual(new InvariantError('refresh token harus string'));
     expect(DomainErrorTranslator.translate(new Error('DELETE_AUTHENTICATION_USE_CASE.NOT_CONTAIN_REFRESH_TOKEN')))
-      .toStrictEqual(new InvariantError('harus mengirimkan token refresh'));
+        .toStrictEqual(new InvariantError('harus mengirimkan token refresh'));
     expect(DomainErrorTranslator.translate(new Error('DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
-      .toStrictEqual(new InvariantError('refresh token harus string'));
+        .toStrictEqual(new InvariantError('refresh token harus string'));
     expect(DomainErrorTranslator.translate(new Error('NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY')))
-      .toStrictEqual(new InvariantError('harus mengirimkan title dan body'));
+        .toStrictEqual(new InvariantError('harus mengirimkan title dan body'));
     expect(DomainErrorTranslator.translate(new Error('NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION')))
-      .toStrictEqual(new InvariantError('title dan body harus string'));
+        .toStrictEqual(new InvariantError('title dan body harus string'));
     expect(DomainErrorTranslator.translate(new Error('NEW_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY')))
-      .toStrictEqual(new InvariantError('harus mengirimkan content'));
+        .toStrictEqual(new InvariantError('harus mengirimkan content'));
     expect(DomainErrorTranslator.translate(new Error('NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION')))
-      .toStrictEqual(new InvariantError('content harus string'));
+        .toStrictEqual(new InvariantError('content harus string'));
     expect(DomainErrorTranslator.translate(new Error('NEW_REPLY.NOT_CONTAIN_NEEDED_PROPERTY')))
-      .toStrictEqual(new InvariantError('harus mengirimkan content'));
+        .toStrictEqual(new InvariantError('harus mengirimkan content'));
     expect(DomainErrorTranslator.translate(new Error('NEW_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION')))
-      .toStrictEqual(new InvariantError('content harus string'));
+        .toStrictEqual(new InvariantError('content harus string'));
   });
 
   it('should return original error when error message is not needed to translate', () => {
