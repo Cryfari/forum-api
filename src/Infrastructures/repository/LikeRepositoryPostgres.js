52111da8a2d2ff307dd6e983c43ceb5082f31367
Pahrijal Saban Mubarok <Kevinsdrajat27@gmail.com>
2023-11-25 03:28:04 +0700

    V2 - fix like count

--- src/Infrastructures/repository/LikeRepositoryPostgres.js
+++ src/Infrastructures/repository/LikeRepositoryPostgres.js
@@ -53,11 +53,11 @@ class LikeRepositoryPostgres extends LikeRepository {
    */
   async getLikeCountOfComment(commentId) {
     const query = {
-      text: 'SELECT COUNT(id) as likecount FROM likes WHERE comment = $1',
+      text: 'SELECT id FROM likes WHERE comment = $1',
       values: [commentId],
     };
     const result = await this._pool.query(query);
-    return result.rows[0].likecount;
+    return result.rowCount;
   }
 }
 
